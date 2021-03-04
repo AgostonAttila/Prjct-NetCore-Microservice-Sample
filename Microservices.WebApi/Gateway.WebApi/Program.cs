7d683149e516fe7114726586ea7f9280dea48989
AgostonAttila <atesz014@gmail.com>
2021-03-04 13:52:41 +0100

    add ocelot

--- Microservices.WebApi/Gateway.WebApi/Program.cs
+++ Microservices.WebApi/Gateway.WebApi/Program.cs
@@ -9,18 +9,24 @@ using System.Threading.Tasks;
 
 namespace Gateway.WebApi
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         CreateHostBuilder(args).Build().Run();
+      }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+      public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+           webBuilder.UseStartup<Startup>();
+        })
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+       config
+       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+       .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+    });
+   }
 }
