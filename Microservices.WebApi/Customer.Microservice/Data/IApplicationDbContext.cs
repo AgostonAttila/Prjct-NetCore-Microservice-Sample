7bcb73a573aecfb57fded3cf6263b19ad8cd497a
AgostonAttila <atesz014@gmail.com>
2021-03-04 13:31:01 +0100

    add 2 Webapi

---
+++ Microservices.WebApi/Customer.Microservice/Data/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Data
+{
+   public interface IApplicationDbContext
+   {
+      DbSet<Entities.Customer> Customers { get; set; }
+      Task<int> SaveChanges();
+   }
+}
---
+++ Microservices.WebApi/Customer.Microservice/Migrations/20210304121855_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Customer.Microservice.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Contact = table.Column<string>(type: "TEXT", nullable: true),
+                    City = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
